f33cb0806e1ce9aa0bbced7aafa486912b2b9e93
jacinta mwangi <jacinta.mwangi@student@moringaschool.com>
2023-02-09 14:56:13 +0300

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header({onDarkModeClick, colorMode}){
+    return(
+        <header>
+			<h2>Shopster</h2>
+			<button onClick={onDarkModeClick}>{colorMode ? "Light" : "Dark"} 
+            Mode</button>
+		</header>
+    );
+}
+
+export default Header;
